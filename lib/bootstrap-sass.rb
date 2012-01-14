2bc671af8423a0ea1896d4572e40c8318359b3ed
Brent Kirby <brent@kurbmedia.com>
2012-01-14 17:57:12 -0500

    init project. add 2.0 submodule to bootstrap core

---
+++ lib/bootstrap-sass.rb
@@ -0,0 +1,7 @@
+require "bootstrap-sass/version"
+
+module Bootstrap
+  module Sass
+    # Your code goes here...
+  end
+end
---
+++ lib/bootstrap/version.rb
@@ -0,0 +1,5 @@
+module Bootstrap
+  module Sass
+    VERSION = "0.0.1"
+  end
+end
